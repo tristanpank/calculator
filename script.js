b39803c6d09f5b2aa3700f8d6c757077e61fbe26
tristanpank <tristanpank@gmail.com>
2022-08-05 15:10:38 -0400

    Finish JS Code and CSS Style

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let currNumbers = [];
 let currentOperator = '';
+let oldOperator = '';
 let display = '';
 let result = 0;
 let operatorClicked = false;
@@ -19,15 +20,20 @@ function main() {
     const divide = document.getElementById('/');
     divide.addEventListener('click', divideButton);
     const clear = document.getElementById('clear');
-    clear.addEventListener('click', clearDisplay);
+    clear.addEventListener('click', clearEverything);
+    const equal = document.getElementById('=');
+    equal.addEventListener('click', equalButton);
 }
 
 function updateDisplay(item) {
     const displayText = document.getElementById('display-text');
-    console.log(this.id)
+    // console.log(this.id)
     if (operatorClicked === true) {
         clearDisplay();
         display = '';
+    }
+    if (currentOperator === '=') {
+        clearEverything();
     }
     displayText.textContent += this.id;
     display += this.id;
@@ -42,30 +48,51 @@ function addResultToDisplay(num) {
 function clearDisplay() {
     const displayText = document.getElementById('display-text');
     displayText.textContent = '';
+    display = '';
+    // currNumbers = [];
+}
+
+function clearEverything() {
+    const displayText = document.getElementById('display-text');
+    displayText.textContent = '';
+    display = '';
+    currentOperator = '';
+    currNumbers = [];
 }
 
 function plusButton(item) {
-    if (currNumbers.length === 0) {
+    if (currentOperator === '=') {
+        currentOperator = '+';
+        return;
+    }
+    else if (currNumbers.length === 0) {
         currNumbers.push(Number(display));
         currentOperator = '+';
-        clearDisplay();
+        operatorClicked = true;
         display = '';
     }
     else if (currNumbers.length === 1) {
+        // currentOperator = '+';
         currNumbers.push(Number(display));
         result = operate(currNumbers, currentOperator);
+        console.log(result);
         addResultToDisplay(result);
         display = result;
         currNumbers = [result];
         operatorClicked = true;
+        currentOperator = '+';
     }
 }
 
 function subtractButton(item) {
-    if (currNumbers.length === 0) {
+    if (currentOperator === '=') {
+        currentOperator = '-';
+        return;
+    }
+    else if (currNumbers.length === 0) {
         currNumbers.push(Number(display));
         currentOperator = '-';
-        clearDisplay();
+        operatorClicked = true;
         display = '';
     }
     else if (currNumbers.length === 1) {
@@ -75,40 +102,100 @@ function subtractButton(item) {
         display = result;
         currNumbers = [result];
         operatorClicked = true;
+        currentOperator = '-';
     }
 }
 
 function multiplyButton(item) {
-    if (currNumbers.length === 0) {
+    if (currentOperator === '=') {
+        currentOperator = '*';
+        return;
+    }
+    else if (currNumbers.length === 0) {
         currNumbers.push(Number(display));
-        currentOperator = '-';
-        clearDisplay();
+        currentOperator = '*';
+        operatorClicked = true;
         display = '';
     }
     else if (currNumbers.length === 1) {
         currNumbers.push(Number(display));
         result = operate(currNumbers, currentOperator);
+        if (!Number.isInteger(result)) {
+            result = Number.parseFloat(result).toFixed(2);
+        }
         addResultToDisplay(result);
         display = result;
         currNumbers = [result];
         operatorClicked = true;
+        currentOperator = '*';
     }
 }
 
 function divideButton(item) {
-    if (currNumbers.length === 0) {
+    if (currentOperator === '=') {
+        currentOperator = '/';
+        return;
+    }
+    else if (currNumbers.length === 0) {
         currNumbers.push(Number(display));
         currentOperator = '/';
-        clearDisplay();
+        operatorClicked = true;
         display = '';
     }
     else if (currNumbers.length === 1) {
-        currNumbers.push(Number(display));
-        result = operate(currNumbers, currentOperator);
-        addResultToDisplay(result);
-        display = result;
-        currNumbers = [result];
-        operatorClicked = true;
+        if (Number(display) == 0) {
+            console.log('error');
+            addResultToDisplay('ERROR');
+            operatorClicked = true;
+            currNumbers = [];
+        }
+        else {
+            currNumbers.push(Number(display));
+            result = operate(currNumbers, currentOperator);
+            if (!Number.isInteger(result)) {
+                result = Number.parseFloat(result).toFixed(2);
+            }
+            addResultToDisplay(result);
+            display = result;
+            currNumbers = [result];
+            operatorClicked = true;
+            currentOperator = '/';
+        }
+    }
+}
+
+function equalButton(item) {
+    if (currentOperator === '=' || operatorClicked === true) {
+        // currNumbers.push(Number(display));
+        // result = operate(currNumbers, oldOperator);
+        // addResultToDisplay(result);
+        // display = result;
+        // currNumbers = [result];
+        // operatorClicked = true;
+        // currentOperator = '=';
+        return;
+    }
+    else if (currNumbers.length === 1) {
+        if (Number(display) === 0 && currentOperator === '/') {
+            console.log('error');
+            addResultToDisplay('ERROR');
+            operatorClicked = true;
+            currNumbers = [];
+        }
+        else {
+            currNumbers.push(Number(display));
+            result = operate(currNumbers, currentOperator);
+            if (!Number.isInteger(result)) {
+                console.log('false');
+                result = Number.parseFloat(result).toFixed(2);
+            }
+            addResultToDisplay(result);
+            display = result;
+            currNumbers = [result];
+            operatorClicked = true;
+            oldOperator = currentOperator;
+            currentOperator = '=';
+        }
     }
 }
 
